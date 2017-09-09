d133878ffa86bf803a8a1c3289f606390755911f
straifer2008 <straifer2008@mail.ru>
2017-09-09 13:27:29 +0300

    save for windows edit to

--- app/js/main.js
+++ app/js/main.js
@@ -5,4 +5,13 @@ $(document).ready(function(){
         nav: true,
         navText: ["&lsaquo;", "&rsaquo;"]
     });
+
+    $('#slider-products').owlCarousel({
+        items: 4,
+        loop: true,
+        nav: false,
+        dots: false,
+        margin: 15,
+        center: true
+    });
 });
