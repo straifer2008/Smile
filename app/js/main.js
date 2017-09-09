4f1ef540732389c65159bdab4c60268f3fbea6a5
straifer2008 <straifer2008@mail.ru>
2017-09-09 16:24:19 +0300

    create desktop version site

--- app/js/main.js
+++ app/js/main.js
@@ -3,15 +3,34 @@ $(document).ready(function(){
         items: 1,
         loop: true,
         nav: true,
-        navText: ["&lsaquo;", "&rsaquo;"]
+        navText: ["&lsaquo;", "&rsaquo;"],
+        autoplay: true
     });
 
     $('#slider-products').owlCarousel({
         items: 4,
         loop: true,
-        nav: false,
+        nav: true,
+        navText : ['<i class="fa fa-angle-left" aria-hidden="true"></i>','<i class="fa fa-angle-right" aria-hidden="true"></i>'],
         dots: false,
         margin: 15,
         center: true
+    });
+
+    $('#slider-clearence').owlCarousel({
+        items: 4,
+        loop: true,
+        nav: false,
+        dots: false,
+        margin: 30
+    });
+
+    $('#slider-brands').owlCarousel({
+        items: 8,
+        nav: false,
+        dots: false,
+        margin: 50,
+        autoWidth: true,
+        startPosition: 5
     });
 });
